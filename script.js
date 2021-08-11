99f3855f4b1522ad5630954034c9a1dab857f7b4
Alexander Hoang <Aleexx.hoang@gmail.com>
2021-08-11 23:39:01 +0200

    Added some more changes, changed the from flex to grid in the div container

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let main = document.getElementById("container");
 
+let refreshMe = document.createElement("button");
+refreshMe.innerHTML = "Click here to refresh";
+refreshMe.classList.add("buttonRefresh");
+
+document.body.appendChild(refreshMe);
+document.body.appendChild(main);
 
 function createGrid() {
     for (let i=0; i<16*16; i++) {
         let grid = document.createElement("div");
         grid.setAttribute("id", "gridStyle");
 
+        grid.addEventListener("mouseover", e => e.target.classList.add("changeColor"))
+
         main.appendChild(grid);
+
     }
     
 } 
+
+
 
 
 
