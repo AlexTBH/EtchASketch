55604dda8e7b6720c9da5e6b386a501eec380b70
Alexander Hoang <Aleexx.hoang@gmail.com>
2021-08-21 23:24:13 +0200

    Made a function so the user can set the amount of rows & columns. Will probably add different colors to mouseover later

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 let main = document.getElementById("container");
 
 let refreshMe = document.createElement("button");
-refreshMe.innerHTML = "Click here to refresh";
+refreshMe.innerHTML = "Set Rows & Columns!";
 refreshMe.classList.add("buttonRefresh");
 
 document.body.appendChild(refreshMe);
 document.body.appendChild(main);
 
-function createGrid() {
-    for (let i=0; i<16*16; i++) {
+function createGrid(column, row) {
+    for (let i=0; i<column*row; i++) {
         let grid = document.createElement("div");
+        main.style.gridTemplateColumns = "repeat(" + column + ", 1fr)";
+        main.style.gridTemplateRows = "repeat(" + row + ", 1fr)";
         grid.setAttribute("id", "gridStyle");
 
-        grid.addEventListener("mouseover", e => e.target.classList.add("changeColor"))
+        grid.addEventListener("mouseover", e => e.target.classList.add("changeColor"));
 
         main.appendChild(grid);
-
     }
-    
 } 
 
+createGrid(16, 16);
 
+function userInput() {
+    main.innerHTML = "";
+    let row = prompt("Enter the amount of rows you would like");
+    let column = prompt("Enter the amount of columns you would like");
+    
 
+    createGrid(row, column);
+}
 
-
-createGrid();
+refreshMe.addEventListener("click", userInput);
